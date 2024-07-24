dd9b8e441982315fab81b4043a288a7f8a8c436b
bharatjoshi <bharat.joshi.bly@gmail.com>
2024-07-24 16:51:37 +0530

    app js demo

---
+++ App.js
@@ -0,0 +1,14 @@
+import {Text, StyleSheet, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hi</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
